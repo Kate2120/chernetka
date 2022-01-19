9c9f198901176ab3b5b2fe1477d3c07c034276ad
Kate2120 <gifos.kate@gmail.com>
2022-01-19 18:17:16 +0200

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Header.module.scss";
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}></div>
+    </div>
+  );
+}
+export default Header;
